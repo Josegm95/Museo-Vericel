9ba3ee8fd2feedeb18761718055371ec6d249b2b
Jose Gomez <jose.fgm1995@gmail.com>
2019-08-24 13:23:41 -0500

    species culicidaes added and modal finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-function cerrar() {}
+const keys = document.getElementsByClassName('key-img');
+const modal = document.getElementById('modal-img');
+
+for (let i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('click', () => {
+    modal.setAttribute('src', keys[i].getAttribute('src'));
+    modal.className = 'modal-img';
+  });
+}
+
+document.getElementById('close-modal').addEventListener('click', () => {
+  modal.setAttribute('src', '');
+  modal.className = 'hide';
+});
